6200f92a58595a686ec63541d54d4fbc7d1c2d43
Abhishek <a1abhishek2016@gmail.com>
2025-05-08 11:04:30 +0530

    Basic routes added

--- ems_frontend/src/App.jsx
+++ ems_frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import './styles/app.css';
-import reactLogo from './assets/react-logo.png';
-import viteLogo from './assets/vite-logo.png';
+import React from "react";
+import "./styles/app.css";
+import reactLogo from "./assets/react-logo.png";
+import viteLogo from "./assets/vite-logo.png";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
-    <div className="app-container">
-      <h1>EMS Frontend using React & Vite</h1>
-      <div className="logos">
-        <img src={viteLogo} alt="Vite Logo" />
-        <img src={reactLogo} alt="React Logo" />
-      </div>
-    </div>
+    <>
+      <AppRoutes />
+    </>
   );
 };
 
